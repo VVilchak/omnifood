44b18333015beeff8acbd1b88346dc08cbaa247b
VVilchak <83465049+VVilchak@users.noreply.github.com>
2022-11-17 21:26:07 +0200

    added smooth scroll

--- script.js
+++ script.js
@@ -7,12 +7,36 @@ yearEl.textContent = currentYear;
 const btnNav = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
-btnNav.addEventListener('click', function() {
-  headerEl.classList.toggle('nav-open')
-})
+btnNav.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 
-// animated scrolling //
+// smooth scrolling //
+const allLinks = document.querySelectorAll("a:link");
 
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
 
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
